6e07a9dce994c510d163e986c0c9dfca2c23746e
RumbleBi <olzlel2000@gmail.com>
2023-04-14 11:56:51 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,52 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    // "plugin:styled-components/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 13,
-    sourceType: 'module',
+    ecmaVersion: 2021,
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
-    'no-console': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    'react/display-name': 'off',
-    'array-callback-return': 'off',
-    'one-var': 'off',
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    indent: ["error", 2],
+    "no-console": "warn",
+    "no-var": "error",
+    "sort-vars": "warn",
+    // "object-property-newline": ["error", "multiline"],
+    // if문 함수를 한줄로 처리하는 방식을 막는다. (에러발생이 안됨..)
+    // error) if(condition) doSomething();
+    // ok)
+    // if (condition) {
+    //   doSomething();
+    // }
+    curly: ["error", "all"],
+    allowSingleLine: 0,
+
+    // 외부 모듈과 내부 모듈을 구분하여 사용한다.(엔터키 공백사용)
+    // ex) import 해서 가져온 모듈과 내부 파일에서 선언한 모듈을 구분
+
+    // 변수, 함수에는 카멜 케이스을 사용한다.
+
+    // URL, HTML 같은 범용적인 대문자 약어는 대문자 그대로 사용한다.
+    // ex) parseHTML, getHTTPRequest
+
+    // "react/react-in-jsx-scope": "off",
+    // "react/prop-types": "off",
+    // "no-unused-vars": "off",
+    // "react/display-name": "off",
+    // "array-callback-return": "off",
+    // "one-var": "off",
   },
-}
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,65 @@
-import 'antd/dist/antd.css'
+import "antd/dist/antd.css";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   ApolloLink,
-} from '@apollo/client'
-import { AppProps } from 'next/app'
-import { Global } from '@emotion/react'
-import { globalStyles } from '../src/commons/styles/globalStyles'
-import Layout from '../src/components/commons/layout'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import { createUploadLink } from 'apollo-upload-client'
+} from "@apollo/client";
+import { AppProps } from "next/app";
+import { Global } from "@emotion/react";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/components/commons/layout";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { createUploadLink } from "apollo-upload-client";
 import {
   createContext,
   Dispatch,
   SetStateAction,
   useEffect,
   useState,
-} from 'react'
-import { getAccessToken } from '../src/commons/libraries/getAccessToken'
-import { onError } from '@apollo/client/link/error'
+} from "react";
+import { getAccessToken } from "../src/commons/libraries/getAccessToken";
+import { onError } from "@apollo/client/link/error";
 
 interface IUserInfo {
-  name?: string
-  email?: string
-  picture?: string
+  name?: string;
+  email?: string;
+  picture?: string;
 }
 interface IGlobalContext {
-  accessToken?: String
-  setAccessToken?: Dispatch<SetStateAction<string>>
-  userInfo?: IUserInfo
-  setUserInfo?: Dispatch<SetStateAction<IUserInfo>>
-  setItem?: Dispatch<SetStateAction<never[]>>
-  item?: never[]
-  isLoggedIn?: boolean
-  setIsLoggedIn?: Dispatch<SetStateAction<boolean>>
+  accessToken?: String;
+  setAccessToken?: Dispatch<SetStateAction<string>>;
+  userInfo?: IUserInfo;
+  setUserInfo?: Dispatch<SetStateAction<IUserInfo>>;
+  setItem?: Dispatch<SetStateAction<never[]>>;
+  item?: never[];
+  isLoggedIn?: boolean;
+  setIsLoggedIn?: Dispatch<SetStateAction<boolean>>;
 }
 
-export const GlobalContext = createContext<IGlobalContext>({})
+export const GlobalContext = createContext<IGlobalContext>({});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log('eslint 작동검사')
-  const [accessToken, setAccessToken] = useState('')
-  const [userInfo, setUserInfo] = useState({})
-  const [item, setItem] = useState([])
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const test = {
+    a: 10,
+    b: 20,
+    c: 30,
+  };
+  const test3 = {
+    a: 20,
+    b: 30,
+    c: 1,
+    d: 12937184718,
+    e: 1883847319,
+    f: 1238718238,
+  };
+
+  console.log("eslint 작동검사");
+  const [accessToken, setAccessToken] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+  const [item, setItem] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const value = {
     accessToken,
     setAccessToken,
@@ -55,45 +69,47 @@ function MyApp({ Component, pageProps }: AppProps) {
     setItem,
     isLoggedIn,
     setIsLoggedIn,
-  }
+  };
   useEffect(() => {
     getAccessToken().then((newAccessToken) => {
-      setAccessToken(newAccessToken)
-    })
-  }, [])
+      setAccessToken(newAccessToken);
+    });
+  }, []);
   // 쿠키 토큰 유효성 검증
 
   const errorLink = onError(({ graphQLErrors, operation, forward }) => {
     // 1. error 캐치
     if (graphQLErrors) {
       for (const err of graphQLErrors) {
-        if (err.extensions.code === 'UNAUTHENTICATED') {
+        if (err.extensions.code === "UNAUTHENTICATED") {
           getAccessToken().then((newAccessToken) => {
-            setAccessToken(newAccessToken)
+            setAccessToken(newAccessToken);
             operation.setContext({
               Headers: {
                 ...operation.getContext().headers,
                 authorization: `Bearer ${newAccessToken}`,
               },
-            })
-            return forward(operation)
-          })
+            });
+            return forward(operation);
+          });
         }
       }
     }
-  })
+  });
 
   const uploadLink = createUploadLink({
-    uri: 'https://backend06.codebootcamp.co.kr/graphql',
-    headers: { Authorization: `Bearer ${accessToken}` }, // HTTP HEADER에 작성해야 accessToken을 사용시 Mutation에서 생성을 할 수 있음.
-    credentials: 'include',
-  })
+    uri: "https://backend06.codebootcamp.co.kr/graphql",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    }, // HTTP HEADER에 작성해야 accessToken을 사용시 Mutation에서 생성을 할 수 있음.
+    credentials: "include",
+  });
 
   const client = new ApolloClient({
     link: ApolloLink.from([errorLink, uploadLink as unknown as ApolloLink]),
     cache: new InMemoryCache(),
     connectToDevTools: true,
-  })
+  });
 
   return (
     <GlobalContext.Provider value={value}>
@@ -104,7 +120,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </Layout>
       </ApolloProvider>
     </GlobalContext.Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
